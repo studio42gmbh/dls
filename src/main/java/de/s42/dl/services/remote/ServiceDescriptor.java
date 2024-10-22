d86bdb158be818865726454a3e61ac68ebe16cdd
Benjamin Schiller <bs@s42m.de>
2024-10-22 13:46:07 +0200

    Throw error if a service defines duplicate method names (has to be unique)

--- src/main/java/de/s42/dl/services/remote/ServiceDescriptor.java
+++ src/main/java/de/s42/dl/services/remote/ServiceDescriptor.java
@@ -1,19 +1,19 @@
 // <editor-fold desc="The MIT License" defaultstate="collapsed">
 /*
  * The MIT License
- * 
+ *
  * Copyright 2022 Studio 42 GmbH ( https://www.s42m.de ).
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -86,6 +86,12 @@ public class ServiceDescriptor implements Comparable<ServiceDescriptor>
 		methodsByName = new HashMap<>();
 
 		for (MethodDescriptor method : methods) {
+
+			// Prevent double names
+			if (methodsByName.containsKey(method.getName())) {
+				throw new RuntimeException("Method with name '" + method.getName() + "' is already mapped in service '" + name + "'");
+			}
+
 			methodsByName.put(method.getName(), method);
 		}
 
